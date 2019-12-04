59b430a74bb939bb1dc149871f55c60d8232633e
Luiz <luizthomazjunior@gmail.com>
2019-12-04 17:57:35 -0300

    first Commit

---
+++ app/src/main/java/br/com/makrosystems/mychat/MainActivity.kt
@@ -0,0 +1,24 @@
+package br.com.makrosystems.mychat
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnListeners()
+    }
+
+    fun btnListeners(){
+        btn_toLogin.setOnClickListener {
+            startActivity(Intent(this, (LoginActivity::class.java)))
+        }
+    }
+
+}
